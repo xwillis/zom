8aa25d5b16949a88ff9a34d10b05b46769419336
xwillis <ximone.willis@gmail.com>
2015-01-04 14:00:24 -0600

    Update gamesetup.py

--- gamesetup.py
+++ gamesetup.py
@@ -1,8 +1,5 @@
 #tell brain which player has been chosen
-var choice = raw_input('choose a player: c, x, j, k, z, m, z')
-while choice !=options
-   choice = raw_input('choose a player: c, x, j, k, z, m, z')
-call brain command
+
 #start with 5 random territories
 #start with 25 troops in each and 10 to deploy
 
