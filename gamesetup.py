cba04f522a0c9dbae971ac1875e6a615e0939a1d
Ximone <Ximone@ximones-air.attlocal.net>
2015-01-04 21:46:12 -0600

    pseudo for difficulty selector

--- gamesetup.py
+++ gamesetup.py
@@ -1,11 +1,14 @@
+#choose difficulty with checkboxes (select these addons for gameplay)
+
+#choose character
+var choice = raw_input('choose a player: c, x, j, k, z, m, z')
+while choice !=options
+    choice = raw_input('you dumb.  choose a player: c, x, j, k, z, m, z')
 #start with 5 random territories
 #start with 25 troops in each and 10 to deploy
 
 #tell brain which territories are occupied by this player
-var choice = raw_input('choose a player: c, x, j, k, z, m, z')
-while choice !=options
-    choice = raw_input('choose a player: c, x, j, k, z, m, z')
-call brain command
 #brain takes over?
+call brain command
 
 #selects a territory with 10 zombies (one for each player)
